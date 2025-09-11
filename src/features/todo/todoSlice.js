4b54d7acfc854bf617a3203b33d81c0c7315f439
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-11 22:01:21 +0500

    updates

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -7,4 +7,16 @@ const initialState = {
 export const todoSlice = createSlice({
   name: "Todo",
   initialState,
+  reducers: {
+    addTodo: (state,action){
+        const todo = {
+            id:nanoid(),
+            text:action.payload
+        }
+        state.todos.push(todo)
+    },
+    removeTodo:(state,action) =>{
+        state.todos =  state.todos.filter((todo) =>todo.id !== action.payload)
+    },
+  }
 });
