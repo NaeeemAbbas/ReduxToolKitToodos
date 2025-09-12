b54c48d4dd74552727a0968e96ffa3c52c419f00
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-12 12:59:37 +0500

    updates

---
+++ src/components/todo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+const todo = () => {
+  return <div></div>;
+};
+
+export default todo;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -8,15 +8,19 @@ export const todoSlice = createSlice({
   name: "Todo",
   initialState,
   reducers: {
-    addTodo: (state,action){
-        const todo = {
-            id:nanoid(),
-            text:action.payload
-        }
-        state.todos.push(todo)
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
     },
-    removeTodo:(state,action) =>{
-        state.todos =  state.todos.filter((todo) =>todo.id !== action.payload)
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-  }
+  },
 });
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
