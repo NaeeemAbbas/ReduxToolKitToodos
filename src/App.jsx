f418a5c69ae8e50e7281fdeb308afa63d277ee77
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-14 20:47:33 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,41 @@ import Todos from "./components/todo";
 function App() {
   return (
     <>
-      <h1>Learn About Redux</h1>
+      <h1>
+        Learn About Redux dfgdfg Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Voluptas, harum dolor minima rerum optio ad voluptatem
+        officia provident labore pariatur in ab, itaque nesciunt! Eaque ipsum
+        eius nostrum corporis iure ipsa quod saepe placeat magni. Quasi, maiores
+        odit! Eaque a impedit est assumenda autem exercitationem! Incidunt
+        repudiandae libero earum a vitae natus, est quos unde fugiat delectus,
+        sit similique pariatur ullam? Quae blanditiis commodi ratione vitae
+        recusandae quidem eum cupiditate, ex in aut rerum deserunt optio totam
+        dolorem repellat officiis aperiam id! Eveniet sunt numquam quod quo
+        aliquam excepturi natus, voluptatem ipsam expedita eos vitae, reiciendis
+        aliquid vero esse a sequi deserunt unde maiores, dolores at consectetur
+        dolore quas praesentium? Blanditiis laudantium amet dolores labore
+        minima, doloribus quod nisi voluptatum, officia, atque eligendi.
+        Laborum, voluptates atque! Libero similique eum ad, architecto nobis
+        molestias quam asperiores voluptate quod dolor eaque illo voluptatem
+        consequuntur neque error dolores. Impedit debitis nulla ipsa sint
+        voluptas aperiam, corrupti, repellat, pariatur illum fugit porro quidem
+        aut. Est impedit quaerat debitis sint rem quidem. Voluptatibus quia,
+        tempora facilis aut doloribus nisi esse earum perspiciatis possimus
+        iusto consequuntur ullam dolore sint eos commodi inventore, est
+        recusandae temporibus reiciendis laboriosam in. Blanditiis, accusamus
+        magni! Maiores saepe, reprehenderit sit dolor itaque error alias eos
+        praesentium? Aspernatur temporibus error fuga nobis fugiat hic magni at
+        impedit placeat. Impedit repellendus, ipsum possimus odit molestias
+        provident saepe reprehenderit. Optio dolorum quo libero vero, temporibus
+        consequatur nostrum ipsum esse iure soluta possimus unde fugit sapiente,
+        voluptate consequuntur repellat delectus sint incidunt beatae cupiditate
+        quia quas. Delectus officia harum sed neque, repellendus officiis!
+        Officiis libero voluptas consequatur in, dolore non maxime, accusamus
+        ullam natus assumenda temporibus! Tempora iure sapiente obcaecati sunt
+        voluptas nostrum in nisi impedit, numquam atque pariatur. Autem iusto
+        expedita vitae assumenda ratione, amet praesentium eum esse optio
+        adipisci, sapiente unde. Incidunt, ducimus?
+      </h1>
       <AddTodo />
       <Todos />
     </>
