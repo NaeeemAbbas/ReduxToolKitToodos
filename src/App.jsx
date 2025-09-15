968452d43fa8e69bad2c12719290c4288b1d9012
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-15 18:20:17 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,53 @@ import Todos from "./components/todo";
 function App() {
   return (
     <>
-      <h1>Learn About Redux </h1>
+      <h1>
+        Learn About Redux Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Quasi itaque tenetur totam ipsum consequatur, eius aut maxime modi
+        minus illo exercitationem excepturi doloribus voluptates pariatur velit
+        dolore reprehenderit, dolores libero optio ut? Iusto iste suscipit unde
+        dolorem numquam veniam ea, repudiandae facere minima, modi sit officia
+        corporis sunt ut. Odit voluptates exercitationem facilis. Commodi, quos
+        dolor iure praesentium at in labore enim delectus quaerat nostrum beatae
+        porro minus pariatur aspernatur, qui, voluptatem totam inventore quam
+        doloribus est obcaecati rerum saepe dolores. Est molestias possimus
+        quaerat nemo dolorem impedit id quo cupiditate recusandae iusto? Nisi
+        officiis ab animi debitis obcaecati dolor sapiente placeat rerum!
+        Corporis quibusdam nobis tempore quisquam non deserunt amet? Consectetur
+        pariatur earum modi corrupti, ex nulla perspiciatis, laborum distinctio
+        aliquid suscipit perferendis sed. Dolorem at ipsam excepturi dolore
+        magni optio voluptatibus libero nostrum laboriosam est, facere facilis
+        non eos. Sint incidunt molestiae perferendis minus natus quisquam
+        repudiandae voluptate deserunt asperiores non animi delectus molestias
+        sequi tempore, consequatur veniam. Alias animi est asperiores, inventore
+        minima et odio qui mollitia eaque iure perferendis a dolore voluptates
+        voluptas beatae porro dolorum rem eveniet magni necessitatibus. Pariatur
+        reiciendis autem delectus! Cumque necessitatibus quam obcaecati
+        asperiores nesciunt saepe laboriosam possimus sapiente dolorem, id ex et
+        facere placeat facilis perspiciatis, eligendi qui nobis a aliquam iure!
+        Unde cum cupiditate laborum minima voluptas ipsam assumenda voluptatibus
+        reprehenderit, nostrum, alias explicabo fuga. Itaque, deserunt,
+        cupiditate obcaecati consequuntur quia id quas libero, quaerat quisquam
+        doloribus ut repellendus ducimus inventore voluptas eos! Assumenda
+        tenetur doloremque labore illo voluptatibus minima vero perferendis
+        aliquid blanditiis commodi, aliquam eius ut magni velit ipsum, illum
+        obcaecati excepturi animi tempore. Temporibus natus doloremque ipsam
+        dolores dignissimos similique atque accusamus. Consequuntur, illo esse
+        reiciendis deserunt possimus voluptas aut commodi rerum cumque illum
+        vitae corrupti! Ipsa laudantium, quod distinctio ex est minima, earum
+        explicabo illo ipsam molestias eos! Laudantium tenetur atque ipsam
+        reprehenderit aut laboriosam perspiciatis quam magni voluptatem
+        molestias cum illo blanditiis praesentium, accusamus natus error eum
+        assumenda beatae minima aperiam nihil laborum veritatis dolore. Eos ut
+        autem facilis veritatis nisi nam consequatur excepturi quidem, dolorum
+        beatae, quibusdam aspernatur pariatur voluptas labore eligendi?
+        Architecto quis consequatur dolores ipsam sit. Assumenda quo dignissimos
+        corporis dicta earum incidunt excepturi dolores iste asperiores sint
+        nihil debitis magni rem ab porro beatae nisi hic maiores, quasi officia
+        sapiente perferendis ad! Sint optio accusamus distinctio aut pariatur,
+        incidunt libero? Obcaecati, qui ipsum assumenda in commodi tempore quam
+        esse dolorem?{" "}
+      </h1>
       <AddTodo />
       <Todos />
     </>
