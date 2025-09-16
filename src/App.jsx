fba60394986ed561b19516799f7c49459a9d12d9
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-09-16 18:35:46 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,65 @@ import Todos from "./components/todo";
 function App() {
   return (
     <>
-      <h1>Learn About Redux lorem400 </h1>
+      <h1>
+        Learn About Redux Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Veritatis earum eligendi ea tempora animi accusamus cum veniam
+        iste expedita. Suscipit deleniti odio quia temporibus beatae ea alias
+        repellendus quasi aspernatur repudiandae. Laborum, tenetur. Cumque,
+        sapiente temporibus. Dolorem impedit repudiandae architecto est dicta
+        ut, odio aliquam cupiditate aperiam dignissimos nesciunt provident
+        error! Placeat vero amet molestias tenetur reprehenderit excepturi
+        corporis odio quas, eius, cum ipsum. Laboriosam doloremque
+        necessitatibus veritatis provident ab dignissimos, nulla impedit quis?
+        Nam eligendi quidem vero laborum deserunt corporis soluta nobis? Laborum
+        a maxime nam dolores, saepe alias nesciunt tempore quae, culpa obcaecati
+        quis totam quos? Et, minima. Mollitia pariatur, commodi inventore velit
+        odit dolor iure repellat molestiae vero porro incidunt aspernatur
+        aliquam tenetur omnis sunt, ipsam laboriosam eveniet praesentium ratione
+        nobis dolores blanditiis atque. Ad fuga mollitia quidem sit eum
+        voluptatibus magnam in odit. Numquam animi suscipit quam sequi mollitia
+        autem itaque rerum nulla officiis ipsa? Maxime, doloribus saepe mollitia
+        tenetur voluptatem minus, id voluptate est assumenda quasi adipisci
+        quas, officiis alias? Assumenda dolorem, vitae doloribus aspernatur,
+        provident magni quas nulla aliquam natus exercitationem et corporis
+        recusandae possimus harum aut beatae sunt veniam veritatis minus qui
+        alias delectus perferendis officiis. Mollitia dignissimos eos, soluta
+        nemo autem beatae facilis veniam doloremque minus! Libero dolore
+        eligendi ipsam corrupti laboriosam maiores corporis exercitationem
+        quibusdam deleniti, aliquam sint sit minus, nemo doloribus iste facere
+        impedit! Obcaecati repellat, quos cupiditate culpa corrupti repellendus
+        aliquam dolorum pariatur sapiente ea. Consectetur similique cumque, est
+        voluptatem enim assumenda quaerat doloremque sed, quisquam veritatis,
+        minus voluptate. Aut quisquam facilis officia laborum magnam ipsam,
+        aliquid soluta iste porro praesentium exercitationem tempora qui, hic
+        molestias impedit nobis delectus! Placeat nesciunt provident ullam atque
+        minus veniam inventore eligendi dicta voluptatum neque corrupti
+        voluptates sequi blanditiis quo iure veritatis, ipsam animi? Quas
+        ducimus incidunt provident commodi aspernatur distinctio nesciunt,
+        corporis nisi natus pariatur explicabo nostrum totam sint neque iure
+        rerum hic delectus. Iure doloribus pariatur, veniam dignissimos
+        similique voluptatibus id, repellendus dolorum doloremque omnis
+        cupiditate ipsum a modi necessitatibus. Sequi animi nisi quis sint
+        veritatis qui perspiciatis incidunt fugit ratione facere vel obcaecati
+        soluta quasi consequuntur repudiandae provident optio dolore,
+        dignissimos cum dolorem sed quia enim! Unde iusto esse exercitationem
+        sequi officiis necessitatibus quam tempore modi, at sit aspernatur, ipsa
+        dignissimos voluptate. Dolor, vitae temporibus? Accusantium, ab libero
+        explicabo voluptates vero atque beatae, ea similique blanditiis, nobis
+        sit tempore facilis sequi aut porro incidunt repellat eaque quia. Omnis
+        delectus in officia? Pariatur hic ut explicabo eaque dolores fugit rerum
+        illum qui corrupti architecto, praesentium facilis cupiditate
+        consequatur ex temporibus molestias. Ratione aperiam quos tempora
+        accusamus labore adipisci minima quas illo ullam veritatis deleniti ut,
+        similique, alias ipsam, modi provident suscipit voluptates quibusdam
+        dignissimos quaerat. Cupiditate, quas temporibus. Recusandae nulla
+        doloremque nam, ad ullam optio, commodi totam laboriosam eos eligendi,
+        consequatur cumque dolores saepe corrupti qui? Placeat tenetur explicabo
+        blanditiis quas, assumenda architecto deserunt veniam aliquid? Impedit
+        aliquam debitis rem recusandae, repellendus sed quidem suscipit saepe
+        expedita quas cupiditate nihil neque placeat quibusdam itaque magni
+        eligendi ratione, soluta dignissimos voluptates autem.{" "}
+      </h1>
       <AddTodo />
       <Todos />
     </>
